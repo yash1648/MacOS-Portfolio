bf93782f82f35eefb4217b96134fe62e931d25ca
yash1648 <yashbagal623@gmail.com>
2025-11-27 14:02:23 +0530

    Implemented the state-management for the apps

---
+++ src/store/Window.jsx
@@ -0,0 +1,33 @@
+import {create} from 'zustand';
+import {immer} from "zustand/middleware/immer";
+import {INITIAL_Z_INDEX, WINDOW_CONFIG} from "#constants/index.js";
+
+const useWindowsStore =create(
+    immer((set)=>(
+        {
+            windows:WINDOW_CONFIG,
+                nextZIndex:INITIAL_Z_INDEX + 1,
+            openWindow:(windowKey,data=null)=> set((state)=>{
+                const win=state.windows[windowKey];
+                win.isOpen=true;
+                win.zIndex=state.zIndex;
+                win.data=data??win.data;
+                state.nextZIndex++;
+            }),
+            closeWindow:(windowKey,data=null)=> set((state)=>{
+                const win=state.windows[windowKey];
+                win.isOpen=false;
+                win.zIndex=INITIAL_Z_INDEX ;
+                win.data=null
+
+            }),
+            focusWindow:(windowKey,data=null)=> set((state)=>{
+                const win=state.windows[windowKey];
+
+                win.zIndex=state.nextZIndex++ ;
+            }),
+        }
+        )))
+
+
+export default useWindowsStore;
